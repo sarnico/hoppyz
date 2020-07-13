ee0a5cf7367d0204bf389657b2a7a66c912e6e86
sara nicolay <nicolay.sara@gmail.com>
2020-07-13 13:04:03 +0200

    start

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def particuliers
+  end
+  def groups
+  end
+  def brasseurs
+  end
+  def restaurateur
+  end
+  def agenda
+  end
+  def contact
+  end
+end
